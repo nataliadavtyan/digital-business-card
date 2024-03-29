33639e22be4fc33bab7ddae38b0c340bfc23b555
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-03-29 09:35:21 +0100

    Center the card

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -12,7 +12,7 @@ export const GlobalStyle = createGlobalStyle`
 
     body {
         display: flex;
-        justify-content: start;
+        justify-content: center;
         align-items: center;
         height: 100vh;
         background: #23252C;
